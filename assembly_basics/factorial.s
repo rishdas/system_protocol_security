49b185144f8d52360d590118970a69d54736050f
rishdas <sirshakdas@gmail.com>
2016-02-12 13:53:22 -0500

    adding comments

--- assembly_basics/factorial.s
+++ assembly_basics/factorial.s
@@ -3,31 +3,31 @@
 
 .section .data
 data:
-.long 5
+.long 5 #the input to the program
 .section .text
 .globl _start	
 _start:
-leal data, %ebx
-movl (%ebx), %ecx
-pushl %ecx	
-call fact
-jmp loop_exit
+leal data, %ebx #store the address of data
+movl (%ebx), %ecx #store the value at data in ecx
+pushl %ecx	#push it into the stack as an argument to the recurisive call
+call fact #call the recursive function
+jmp loop_exit #exit
 	
 .type fact, @function
 fact:
 pushl %ebp #save the base pointer
 movl %esp, %ebp	#make the base pointer the value of the stack pointer
-subl $4, %esp	
-movl 8(%ebp), %ebx	
-movl %ebx, -4(%ebp)
-cmpl $1, %ebx
-je no_recurse	
-decl %ebx
-pushl %ebx
-call fact
-popl %edx
-incl %edx	
-imull %edx, %ebx
+subl $4, %esp	#make space for local variables
+movl 8(%ebp), %ebx #load the argument in ebx
+movl %ebx, -4(%ebp) #save it as a local variable
+cmpl $1, %ebx #check if argument is 1 then just return
+je no_recurse	#check if argument is 1 then just return
+decl %ebx #decrement the ebx 
+pushl %ebx #push the decremneted value as argument
+call fact # call fact recursively
+popl %edx # extract the argument
+incl %edx # increment it again
+imull %edx, %ebx #multiply the return value with your original argument
 
 no_recurse:
 movl %ebp, %esp #restore the stack pointer
